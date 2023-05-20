98dc039ddff8bc0aaab2a3e1a2bedf357b6fbc37
pancracio3166 <99661263+pancracium@users.noreply.github.com>
2023-05-20 22:07:30 +0200

    Added custom click sounds, highest score and other minor implementations.

--- main.py
+++ main.py
@@ -42,7 +42,10 @@ Notes:
 
 - If the update delay value is zero or a negative number, the program will likely crash. This is not a bug, it's common sense.
 
-- Even if you use the clicking method of an achivement, you may get different CPS than what the achievement shows.
+- Even if you use the clicking method of an achivement, you may get different CPS than what the achievement shows. For example even if you drag click you may not get 23-30 CPS.
+
+- If you set the duration to a number less than 1, you will get an incorrectly high amount of cps. This is because the click count is divided by the duration. For example, 
+1 (Click count) / 0.1 (duration in seconds) is 10, and so on.
 
 
 I don't think I have to describe what each widget does, it pretty self-explainative.
@@ -50,9 +53,11 @@ I don't think I have to describe what each widget does, it pretty self-explainat
 Created by pancracium @ GitHub (https://github.com/pancracium/cps-test).
 """
 
+#Import the necessary modules and call pygame.init() to initilize pygame
 import json, pygame, time
 import tkinter as tk
 import tkinter.messagebox as msgbox
+from tkinter import filedialog
 from tkinter import ttk
 pygame.init()
 
@@ -64,23 +69,28 @@ class CPS_Test:
         self.master.geometry("1080x720+420+180")
         self.master.iconbitmap("icon.ico")
 
-        #Save the gained awards to the awards.json file when the window is closed
-        self.master.protocol("WM_DELETE_WINDOW", self.save_awards)
+        #Save the user's configuration to the config.json file when the window is closed
+        self.master.protocol("WM_DELETE_WINDOW", lambda: self.save_config(close=True))
 
         #Define some fonts
         self.click_font = ("Roboto", 20, "bold")
         self.font = ("Roboto", 11, "bold")
 
-        #Declare some necessary variables
+        #Info for the CPS test result
         self.click_count = 0
         self.start_time = None
         self.end_time = None
         self.update_delay = 10
-        self.click_sound = pygame.mixer.Sound("click.wav")
+
+        #Load the click sound
+        self.click_sound = "click.wav"
+
+        #Awards and highest score
         self.awards = []
+        self.highest_score = 0
 
         #Call the create_widgets() and update() methods
-        self.load_awards()
+        self.load_config()
         self.create_widgets()
         self.update()
 
@@ -91,9 +101,10 @@ class CPS_Test:
             text="Enter test duration in seconds:", font=self.font)
         self.label_instructions.place(relx=0.5, rely=0.025, anchor=tk.CENTER)
 
-        #Entry for the duration in seconds of the test
+        #Entry for the duration in seconds of the test and set 10 by default
         self.entry_duration = ttk.Entry(font=self.font)
         self.entry_duration.place(relx=0.5, rely=0.055, anchor=tk.CENTER)
+        self.entry_duration.insert(0, "10")
 
         #Button to start the test
         self.button_start = ttk.Button(text="Start Test",
@@ -136,7 +147,7 @@ class CPS_Test:
         #Entry for the update delay
         self.entry_delay = ttk.Entry(font=self.font, width=15)
         self.entry_delay.place(relx=0.1, rely=0.95, anchor=tk.CENTER)
-        self.entry_delay.insert(0, "10")
+        self.entry_delay.insert(0, str(self.update_delay))
 
         #Button to update the update delay
         self.button_delay = ttk.Button(text="Change update delay",
@@ -148,40 +159,53 @@ class CPS_Test:
                                         command=self.show_awards)
         self.button_awards.place(relx=0.9, rely=0.9, anchor=tk.CENTER)
 
+        #Button to change the click sound to a custom one
+        self.button_sound = ttk.Button(text="Upload custom click sound", command=self.change_click_sound)
+        self.button_sound.place(relx=0.9, rely=0.95, anchor=tk.CENTER)
+
     def start_test(self):
         """Start the test."""
         #Get the duration the user submitted
         duration = self.entry_duration.get()
 
         try:
-            #Convert the duration (string) into a float (number with decimal point)
-            self.duration = float(duration)
+            #Add a keyword to reset all the awards easier
+            if duration == "resetawards":
+                self.awards = []
+                self.highest_score = 0
+                msgbox.showinfo(title="Success",
+                                message="Successfully reset all awards.")
 
-            #Show the user an error message if the number is negative
-            if self.duration < 0:
-                raise ValueError
+            else:
+                #Convert the duration (string) into a float (number with decimal point)
+                self.duration = float(duration)
 
-            #Tell the user the test will be infinite if the duration is zero
-            if self.duration == 0:
-                msgbox.showinfo(
-                    title="Infinite clicking test",
-                    message=
-                    "Note that if you insert zero as duration, the test will be infinite, giving no CPS results."
-                )
+                #Show the user an error message if the number is negative
+                if self.duration < 0:
+                    raise ValueError
 
-            #Convert the duration to a number, tell the user the test has started and change some buttons' states and add a click to the click count
-            self.label_instructions.config(text="Click as fast as you can!")
-            self.entry_duration.config(state=tk.DISABLED)
-            self.button_start.config(state=tk.DISABLED)
-            self.button_click.config(state=tk.NORMAL)
-            self.button_end.config(state=tk.NORMAL)
-            self.click_count = 1
+                #Tell the user the test will be infinite if the duration is zero
+                if self.duration == 0:
+                    msgbox.showinfo(
+                        title="Infinite clicking test",
+                        message=
+                        "Note that if you insert zero as duration, the test will be infinite, giving no CPS results."
+                    )
+
+                #Convert the duration to a number, tell the user the test has started and change some buttons' states and add a click to the click count
+                self.label_instructions.config(text="Click as fast as you can!")
+                self.entry_duration.config(state=tk.DISABLED)
+                self.button_start.config(state=tk.DISABLED)
+                self.button_click.config(state=tk.NORMAL)
+                self.button_end.config(state=tk.NORMAL)
+                self.click_count = 1
 
         except (ValueError, TypeError):
             #Show the user an error message if the inserted duration is not a valid
-            message = "Please insert a positive number (it can also be a float) as duration, i. e.: 10, 5.2, 0.5, 100, 0, ...\n"
-            message += "Also note you should use a point (.) and not a comma (,) as decimal point."
-            msgbox.showerror(title="Error", message=message)
+            msgbox.showerror(
+                title="Error", 
+                message="Please insert a positive number (it can also be a float) as duration, i. e.: 10, 5.2, 0.5, 100, 0, ...\n"
+            )
             self.entry_duration.delete(0, tk.END)
 
     def click(self):
@@ -194,7 +218,7 @@ class CPS_Test:
             self.click_count += 1
             self.label_clicks.config(
                 text="Clicks: {}".format(self.click_count))
-            self.click_sound.play()
+            pygame.mixer.Sound(self.click_sound).play()
 
     def update(self):
         """Update the program."""
@@ -281,14 +305,24 @@ class CPS_Test:
                     message=
                     "You gained the \"Autoclicker clicker (get 41+ CPS in a test)\" award!"
                 )
+            
+            #Check if the user has beaten his highest score
+            if cps > self.highest_score:
+                previous_highest_score = self.highest_score
+                self.highest_score = cps
+                msgbox.showinfo(
+                    title="New highest score!",
+                    message=f"You've beaten your previous CPS record of {previous_highest_score} CPS, with {self.highest_score} CPS!"
+                )
 
             #Show the CPS to the user
             self.label_instructions.config(
                 text="Your CPS is: {:.2f}".format(cps))
             self.button_click.config(text="{:.2f} CPS".format(cps))
 
-            #Disable the Click button and enable the new test button again
+            #Disable the Click button and the end button and enable the new test button again
             self.button_click.config(state=tk.DISABLED)
+            self.button_end.config(state=tk.DISABLED)
             self.button_new_test.config(state=tk.NORMAL)
         else:
             #Calculate elapsed time and update the timer label
@@ -365,6 +399,7 @@ class CPS_Test:
         #Create a new toplevel window
         awards_window = tk.Toplevel()
         awards_window.title("Current awards")
+        awards_window.iconbitmap("awards.ico")
         awards_window.geometry("400x300+760+390")
 
         #Create a new string where all the gained awards will be shown
@@ -372,24 +407,86 @@ class CPS_Test:
             current_awards = "Not gained any award yet. Start a new test to gain new awards!"
         else:
             current_awards = "\n".join("- " + award for award in self.awards)
+        
+        #Create a label to show the user's highest score
+        highest_score_label = tk.Label(awards_window, text=f"highest score: {self.highest_score} CPS")
+        highest_score_label.pack()
 
         #Create a label to show the string with the awards
         awards_label = tk.Label(awards_window, text=current_awards)
         awards_label.pack()
 
-    def save_awards(self):
-        """Save the awards to the awards.json file."""
-        with open('awards.json', 'w') as file:
-            json.dump(self.awards, file)
-        self.master.destroy()
+        #Create a button to close the toplevel window
+        ok_button = ttk.Button(awards_window, text="OK", command=awards_window.destroy)
+        ok_button.pack()
 
-    def load_awards(self):
-        """Load the awards from the awards.json file."""
+    def save_config(self, close:bool):
+        """Save the user's configuration to the config.json file."""
+        #Create a dictionary for better organization
+        data = {
+            "awards": self.awards,
+            "highest_score": self.highest_score,
+            "click_sound": self.click_sound,
+            "update_delay": self.update_delay
+        }
+
+        #Dump all the data to the config.json file
+        with open("config.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+        #Close the program if the close parameter is true
+        if close:
+            self.master.destroy()
+
+    def load_config(self):
+        """Load the user's configuration from the config.json file."""
         try:
-            with open('awards.json', 'r') as file:
-                self.awards = json.load(file)
+            #Try to get the data from the config.json
+            with open("config.json", "r") as file:
+                data = json.load(file)
+                self.awards = data.get("awards", [])
+                self.highest_score = data.get("highest_score", 0)
+                self.click_sound = data.get("click_sound", "click.wav")
+                self.update_delay = data.get("update_delay", 10)
         except FileNotFoundError:
+            #If the files doesn't exist, set some default values
             self.awards = []
+            self.highest_score = 0
+            self.click_sound = "click.wav"
+            self.update_delay = 10
+
+    def change_click_sound(self):
+        """Changes the click sound to a custom one by uploading a WAV or MP3 file."""
+        #Prompt the user for a file
+        file = filedialog.askopenfilename(
+            title="Select custom click sound",
+            initialdir="~/Music",
+            filetypes=(("WAV files", "*.wav"), 
+                       ("MP3 files", "*.mp3"), 
+                       ("All files", "*.*")),
+        )
+
+        if file:
+            #Get only the file name, not the complete directory
+            file_name = [x for x in file.split("/")][-1]
+
+            #Change the click sound to the new one if there aren't any errors
+            try:
+                self.click_sound = file
+            except pygame.error as e:
+                #Show the user an error message if there's an error
+                msgbox.showerror(
+                    title="Error", 
+                    message=f"Error: {e}"
+                )
+            else:
+                #Else, tell the user everything's fine
+                msgbox.showinfo(
+                    title="Success", 
+                    message=f"Successfully changed the click sound to {file_name}"
+                )
+            
+            self.save_config(close=False)
 
     def run(self):
         """Run the program"""
